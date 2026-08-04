12e90d59ca87c7df29629ce3db37943cb7168bcc
Vaishali Bhardwaj <vaishalibhardwaj42454@gmail.com>
2026-08-04 09:25:36 +0530

    Add new physics files

---
+++ Roots_of_Quadratic_equation.py
@@ -0,0 +1,20 @@
+import math
+def find_roots(a,b,c):
+    d = b**b - 4*a*c
+    if d>a :
+        root1 = (-b + math.sqrt(d)/2*a)
+        root2 = (-b - math.sqrt(d)/2*a)
+        return f"Two distinct real roots are :{root1},{root2}"
+    elif d==0 :
+        root = -b/(2*a)
+    else :
+        real_part = -b/(2*a)
+        img_part = math.sqrt(-d)/(2*a)
+        return f"Complex roots are : {real_part}+{img_part}i,{real_part}-{img_part}i"
+
+print("Quadratic equation solver.")
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+c = int(input("Enter c:"))
+
+print(find_roots(a,b,c))
